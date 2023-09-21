496e47f6b5add768428e431fb58f20866fa11ffb
Jagomez95 <144792662+Jagomez95@users.noreply.github.com>
2023-09-21 17:54:46 +0200

    Create ft_strlcat.c

---
+++ ft_strlcat.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jagomez <jagomez@student.42madrid.>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/18 19:12:28 by jagomez           #+#    #+#             */
+/*   Updated: 2023/09/19 16:53:48 by jagomez          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	londst;
+	size_t	lonsrc;
+	size_t	cont;
+
+	londst = ft_strlen(dst);
+	lonsrc = ft_strlen(src);
+	if (dstsize <= londst)
+		return (lonsrc + dstsize);
+	cont = londst;
+	while (*src != '\0' && cont < (dstsize - 1))
+		*(dst + cont++) = *src++;
+	*(dst + cont) = '\0';
+	return (londst + lonsrc);
+}
