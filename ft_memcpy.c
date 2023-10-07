ed16b45391351a6b54f041c32d513f49952db933
Jagomez95 <144792662+Jagomez95@users.noreply.github.com>
2023-10-07 19:16:07 +0200

    Update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -28,3 +28,16 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	}
 	return (dst);
 }
+int main() {
+    // Ejemplo: Copiar una cadena a otra
+    char fuente[] = "Hola, mundo";
+    char destino[20];
+
+    // Utilizar ft_memcpy para copiar la cadena fuente a la cadena destino
+    ft_memcpy(destino, fuente, sizeof(fuente));
+
+    // Imprimir el contenido del destino
+    printf("Destino: %s\n", destino);
+
+    return 0;
+}
