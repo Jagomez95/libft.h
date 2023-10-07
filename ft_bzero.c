4bda792a8350f78f023ab73e2cc4bf5175b06aae
Jagomez95 <144792662+Jagomez95@users.noreply.github.com>
2023-10-07 18:30:50 +0200

    Update ft_bzero.c

--- ft_bzero.c
+++ ft_bzero.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include "libft.h"
 
 void	ft_bzero(void *s, size_t n)
 {
@@ -22,4 +22,15 @@ void	ft_bzero(void *s, size_t n)
 		((char *)s)[i] = 0;
 		i++;
 	}
+}
+int main() {
+    char cadena[10];
+	
+    // Utilizar ft_bzero para establecer los primeros 5 bytes de la cadena a cero
+    ft_bzero(cadena, 5);
+
+    // Imprimir la cadena después de ft_bzero
+    printf("Cadena después de ft_bzero: %s\n", cadena);
+
+    return 0;
 }
