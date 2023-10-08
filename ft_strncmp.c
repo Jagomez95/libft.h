6d60c321f9285daf74d70167e6ef48f1b0626360
Jagomez95 <144792662+Jagomez95@users.noreply.github.com>
2023-10-08 18:05:37 +0200

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -10,24 +10,29 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include "libft.h"
 
-int	ft_strncmp(char *s1, char *s2, unsigned int n)
+int     ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	unsigned int	i;
+    while ((*s1 || *s2) && n--)//continuará mientras al menos uno de los caracteres en s1 o s2 sea no nulo (*s1 o *s2 sea verdadero), y aún haya caracteres por comparar (n sea mayor que 0).
+        if (*s1++ != *s2++)//compara los caracteres s1 y s2 a medida se incrementa
+		//la funcion retorna la diferencia entre los valores ASCII
+            return ((unsigned char)*--s1 - (unsigned char)*--s2);//si es diferente, se retorna la dif y retroceden una posicion para apuntar el caracter que causo la diferencia
+    return (0);
+}
+int main() {
+    const char *cadena1 = "Hola";
+    const char *cadena2 = "Hola, mundo";
 
-	i = 0;
-	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
-	{
-		if (s1[i] > s2[i])
-		{
-			return (1);
-		}
-		if (s1[i] < s2[i])
-		{
-			return (-1);
-		}
-		i++;
-	}
-	return (0);
+    int resultado = ft_strncmp(cadena1, cadena2, 4);
+
+    if (resultado == 0) {
+        printf("Las cadenas son iguales en los primeros caracteres.\n");
+    } else if (resultado < 0) {
+        printf("La cadena1 es menor que la cadena2.\n");
+    } else {
+        printf("La cadena1 es mayor que la cadena2.\n");
+    }
+
+    return 0;
 }
