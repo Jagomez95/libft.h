aab555165e0cbfd834b804cbf854b2ded0f68f3d
Jagomez95 <144792662+Jagomez95@users.noreply.github.com>
2023-10-09 17:47:38 +0200

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -18,18 +18,31 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	j;
 
 	i = 0;
-	if (*needle == '\0' || needle == NULL)
-		return ((char *)haystack);
-	while (haystack[i] != '\0' && i < len)
+	if (*needle == '\0' || needle == NULL)//comprueba que sea una cadena vacia o un puntero nulo.
+		return ((char *)haystack);//devuelve un puntero al comienzo de la cadena haystack
+	while (haystack[i] != '\0' && i < len)//Inicia un bucle que recorre la cadena haystack hasta que se llega al final de la cadena o se alcanza la longitud máxima especificada por len.
 	{
 		j = 0;
-		while (needle[j] == haystack[i + j] && i + j < len)
-		{
-			if (needle[j + 1] == '\0')
-				return ((char *)haystack + i);
+		while (needle[j] == haystack[i + j] && i + j < len)//Inicia un bucle que compara los caracteres de needle con los caracteres de haystack comenzando desde la posición i. El bucle se ejecuta mientras los caracteres coincidan y no se haya alcanzado la longitud máxima len.		{
+			if (needle[j + 1] == '\0')//Si se encuentra una coincidencia completa de la subcadena needle en la posición i de haystack, la función devuelve un puntero al comienzo de la coincidencia en haystack.				return ((char *)haystack + i);
 			j++;
 		}
 		i++;
 	}
 	return (NULL);
+}
+int main() {
+    const char *haystack = "This is a sample string.";
+    const char *needle = "sample";
+    size_t len = 10;
+
+    char *result = ft_strnstr(haystack, needle, len);
+
+    if (result != NULL) {
+        printf("Encontrado: %s\n", result);
+    } else {
+        printf("No encontrado\n");
+    }
+
+    return 0;
 }
