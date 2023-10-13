c75bfa0165a380e3033327084a58b79b874edfa3
Jagomez95 <144792662+Jagomez95@users.noreply.github.com>
2023-10-13 19:16:07 +0200

    Update ft_putnbr_fd.c

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -17,16 +17,18 @@ void	ft_putnbr_fd(int n, int fd)
 	long int	copia;
 
 	copia = n;
+// Verifica si copia es negativo. Si es así, ajusta copia a su valor absoluto multiplicándolo por -1 y escribe el signo '-' en el archivo.
 	if (copia < 0)
 	{
 		copia = (copia * - 1);
 		write (fd, "-", 1);
 	}
+// Verifica si el valor absoluto de copia es mayor que 9. Si es así, realiza una llamada recursiva a ft_putnbr_fd para imprimir los dígitos.
 	if (copia > 9)
 	{
-		ft_putnbr_fd (copia / 10, fd);
-		ft_putchar_fd ((copia % 10) + '0', fd);
+		ft_putnbr_fd (copia / 10, fd); //Llamada recursiva para imprimir el dígito de la decena.
+		ft_putchar_fd ((copia % 10) + '0', fd); //Imprime el dígito de la unidad.
 	}
 	else
-		ft_putchar_fd (copia + '0', fd);
+		ft_putchar_fd (copia + '0', fd);//Si el valor absoluto de copia es menor o igual a 9, imprime el dígito directamente.
 }
