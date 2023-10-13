b53cea8bfa1cd4383bd88f4d7da7a4e726947db1
Jagomez95 <144792662+Jagomez95@users.noreply.github.com>
2023-10-13 15:32:01 +0200

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,24 +1,30 @@
 #include "libft.h"
 
-int     ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-    int num;// para almacenar el valor entero resultante 
-    int sign;// para almacenar el signo del número.
-    int i;
+	int					i;
+	int					sign;
+	unsigned long long	result;
 
-    num = 0;
-    i = 0;
-    sign = 1;//en 1 antes de comenzar el procesamiento de la cadena.
-    while (str[i] == ' ' || str[i] == '\r' || str[i] == '\t'
-    		|| str[i] == '\n' || str[i] == '\v' || str[i] == '\f')
+	result = 0;
+	sign = 1;
+	i = 0;
+	while (str[i] == ' ' || str[i] == '\r' || str[i] == '\t'
+			|| str[i] == '\n' || str[i] == '\v' || str[i] == '\f')
 		i++;
-    if (*str == '-')//Si el primer carácter después de los espacios en blanco es '-', entonces se establece sign en -1.
-        sign = - 1;
-    if (*str == '-' || *str == '+')//Si el primer carácter después de los espacios en blanco es '-' o '+', entonces se avanza el puntero str al siguiente carácter.
-        str++;
-    while (*str >= '0' && *str <= '9')// Este bucle while se ejecuta mientras el carácter actual es un dígito.
-        num = num * 10 + *str++ - '0';//Este código actualiza num multiplicándolo por 10 (desplazamiento a la izquierda) y sumándole el valor numérico del dígito actual. El puntero str se incrementa para pasar al siguiente carácter.
-    return (num * sign);//La función devuelve el valor entero resultante, multiplicado por el signo que se determinó anteriormente.
+	if (str[i] == '-')
+		sign *= -1;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		if (result > __LONG_LONG_MAX__)
+			return (sign == -1 ? 0 : -1);
+		result *= 10;
+		result += (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
 }
 int main() {
     // Ejemplo 1: cadena con dígitos positivos
