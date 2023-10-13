48db06b9300c11218ea955fa8b1220d0a41a6488
Jagomez95 <144792662+Jagomez95@users.noreply.github.com>
2023-10-13 16:25:26 +0200

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -20,20 +20,43 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	i = 0;
 	j = 0;
+	//Asigna dinámicamente memoria para la nueva cadena. El tamaño es la suma de las longitudes de las cadenas más uno para el carácter nulo \0 al final.
 	str = (char *) malloc(sizeof(*s1) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	//Verifica si la asignación de memoria fue exitosa. Si no lo fue, devuelve NULL.
 	if (!str)
 		return (NULL);
+	//Copia los caracteres de la primera cadena (s1) a la nueva cadena (str) y actualiza los índices.
 	while (s1[i])
 	{
 		str[j++] = s1[i];
 		i++;
 	}
+	// Repite el proceso para la segunda cadena (s2), agregando sus caracteres a la nueva cadena.
 	i = 0;
 	while (s2[i])
 	{
 		str[j++] = s2[i];
 		i++;
 	}
-	str[j] = 0;
+	str[j] = 0;//Agrega el carácter nulo \0 al final de la nueva cadena.
 	return (str);
+}
+int main() {
+    const char *cadena1 = "Hola, ";
+    const char *cadena2 = "mundo!";
+
+    // Llamar a la función ft_strjoin para concatenar las cadenas
+    char *resultado = ft_strjoin(cadena1, cadena2);
+
+    if (resultado != NULL) {
+        // Imprimir el resultado
+        printf("Cadena concatenada: %s\n", resultado);
+
+        // Liberar la memoria asignada por ft_strjoin
+        free(resultado);
+    } else {
+        printf("Error al concatenar las cadenas.\n");
+    }
+
+    return 0;
 }
