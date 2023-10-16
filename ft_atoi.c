d7f3919268aca11633852b1b9acfbfac164f7ac5
Jagomez95 <144792662+Jagomez95@users.noreply.github.com>
2023-10-16 13:47:51 +0200

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -2,29 +2,31 @@
 
 int	ft_atoi(const char *str)
 {
-	int					i;
-	int					sign;
-	unsigned long long	result;
+	int	i;
+	int	neg;
+	int	res;
 
-	result = 0;
-	sign = 1;
 	i = 0;
-	while (str[i] == ' ' || str[i] == '\r' || str[i] == '\t'
-			|| str[i] == '\n' || str[i] == '\v' || str[i] == '\f')
+	neg = 1;
+	res = 0;
+	if (!str)
+		return (0);
+	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v' || \
+			str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
 		i++;
 	if (str[i] == '-')
-		sign *= -1;
-	if (str[i] == '-' || str[i] == '+')
+	{
+		neg = -1;
+		i++;
+	}
+	else if (str[i] == '+')
 		i++;
 	while (str[i] >= '0' && str[i] <= '9')
 	{
-		if (result > __LONG_LONG_MAX__)
-			return (sign == -1 ? 0 : -1);
-		result *= 10;
-		result += (str[i] - '0');
+		res = (res * 10) + (str[i] - '0');
 		i++;
 	}
-	return (result * sign);
+	return (res * neg);
 }
 int main() {
     // Ejemplo 1: cadena con dígitos positivos
