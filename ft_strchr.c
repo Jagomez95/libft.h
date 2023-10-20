ed1f65cc941c5102998eadcc312457f7febf7449
Jagomez95 <144792662+Jagomez95@users.noreply.github.com>
2023-10-20 14:00:53 +0200

    Update ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -14,24 +14,18 @@
 
 char	*ft_strchr(const char *str, int c)
 {
-	int	i;
-	int	flag;//Se declara una variable entera flag que se utiliza para indicar si se ha encontrado el carácter.
+	char	a;
 
-	flag = 0;
-	i = 0;
-	//Inicia un bucle while. El bucle se ejecutará mientras el carácter en la posición i de la cadena no sea el carácter nulo ('\0') y flag sea igual a 0.
-	while (((char *)str)[i] != '\0' && flag == 0)
+	a = c;
+	while (*str)
 	{
-		if (((char *)str)[i] == c)//Comprueba si el carácter en la posición i de la cadena es igual al carácter c.
-		{
-			return ((char *)&str[i]);//Si se encuentra el carácter, devuelve un puntero a la posición en la que se encontró. La conversión de tipo (char *)&str[i] se realiza para cambiar el tipo de constante a no constante.
-			flag = 1;//eliminar esta linea
-		}
-		i++;
+		if (*str == a)
+			return ((char *)str);
+		str++;
 	}
-	if (c == '\0')//Verifica si el carácter c es el carácter nulo ('\0').
-		return ((char *)&str[i]);//Si c es el carácter nulo, devuelve un puntero al final de la cadena.
-	return (NULL);
+	if (a == '\0')
+		return ((char *)str);
+	return (0);
 }
 int main() {
     const char *cadena = "Hola, mundo";
