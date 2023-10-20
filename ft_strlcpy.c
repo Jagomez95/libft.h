b1f8a0dedc4ff47857cb7a2fa99ff2815e449581
Jagomez95 <144792662+Jagomez95@users.noreply.github.com>
2023-10-20 14:40:31 +0200

    Update ft_strlcpy.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -17,19 +17,26 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	size_t	i;
 
 	i = 0;
-	if (dstsize == 0)
+	if (dstsize == 0)//verifica si el tamaño de dstsize es cero. 
+//Si es así, entra en este bloque de código para calcular la longitud de la cadena de origen (src) y devuelve ese valor. 
 	{
 		while (src[i])
 			i++;
 		return (i);
 	}
+//Este bucle copia caracteres desde la cadena de origen (src) a la cadena de destino (dst). El bucle se detiene si se alcanza 
+//el tamaño límite (dstsize - 1) o si se encuentra el carácter nulo en src.
 	while (i < dstsize - 1 && src[i] != '\0')
 	{
 		dst[i] = src[i];
 		i++;
 	}
+//Si el índice i es menor que el tamaño límite, agrega un carácter nulo al final de la cadena de destino (dst). 
+//Esto asegura que la cadena de destino esté correctamente terminada.
 	if (i < dstsize)
 		dst[i] = '\0';
+//Después de salir del bucle anterior, este bucle calcula la longitud total de la cadena de origen (src) sin truncar, 
+//ya que se ha llegado al límite del destino.
 	while (src[i] != '\0')
 		i++;
 	return (i);
